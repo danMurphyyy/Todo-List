e833c1ecfec5d5c425f467d30437e6c272c245b4
danMurphyyy <dan.murphy250317@gmail.com>
2025-09-12 00:56:47 +0100

    almost got todo creation working

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import Project from "./project";
 import TodoItem from "./todo-items";
 import Popup from "./popup";
+import UI from "./ui";
 import "./styles.css";
 
-const defaultProject = new Project("Default");
-
-const myTodo = new TodoItem("clean Room", "clean it all", "2025-09-15", "high-priority");
-
-defaultProject.addTodo(myTodo);
-
-console.log(defaultProject);
-
 const root = document.getElementById("content");
-root.innerHTML = "<p>Hello from Webpack Starter 🚀</p>";
+const ui = new UI(content);
+
+const todoPopup = new Popup({
+  popupId: "todo-popup",
+  openBtnId: "create-todo-btn",
+  closeBtnId: "todo-cancel",
+  saveBtnId: "todo-save",
+});
+
+document.getElementById("todo-save").addEventListener("click", () => {
+  const todo = todoPopup.save();
+  if(todo) ui.renderTodo(todo);
+})
